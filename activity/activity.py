160f5ab90448b4e5f015901ff1dfb7bb49de4278
Tram <tramnhoang@gmail.com>
2024-11-13 18:12:04 -0800

    solution

--- activity/activity.py
+++ activity/activity.py
@@ -3,4 +3,23 @@
 # code.
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
-        pass
+        # Filter out punctuation
+        filtered_paragraph = "".join([c if c.isalpha() or c.isspace else " " for c in paragraph])
+
+        # get words from paragraph
+        words = filtered_paragraph.split()
+
+        # filter out a word if it is banned
+        banned_set = set(banned)
+        valid_words = [word for word in words if word not in banned_set]
+
+        # count each word occurence 
+        counts = {}
+        for word in valid_words:
+            counts[word] = counts.get(word, 0) + 1
+        # find the word with the highest count:
+
+        max_word = max(counts, key=counts.get)
+
+        # Return the word:
+        return max_word
